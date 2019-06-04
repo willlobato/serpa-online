2b9b77562fb15f8f586c9a8fca5fbc01322722a5
willlobato <will.lobato@gmail.com>
2019-06-03 23:12:25 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { MenuComponent } from './ui/menu/menu.component';
+import { HomeComponent } from './ui/home/home.component';
+import { FooterComponent } from './ui/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
